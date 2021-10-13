c9df5fe7f1923f875fe738be1b77b4df2220e187
liruiqing <liruiqing7@163.com>
2021-10-13 08:08:56 +0800

    learing loader

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import avatar from './avatar.jpg';
+import './index.css'
 
 var img = new Image();
 img.src = avatar;
+img.classList.add('avatar');
 
 var root = document.getElementById('root');
 root.append(img);
